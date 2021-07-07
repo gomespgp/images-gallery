7ab97d60f12617a395e2234ab9dee5019f26e009
Gabriel Gomes <gabriel_p_g@hotmail.com>
2021-07-07 20:59:32 -0300

    reset search input upon submit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,19 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import Search from "./components/Search";
 
+// Unsplash API key
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
-  const [word, setWord] = useState('');
+  const [word, setWord] = useState("");
 
   const handleSearchSubmit = (e) => {
+    // Avoinding browser to reload
     e.preventDefault();
-    console.log(word);
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    // Fetching word random image from Splash
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
@@ -19,6 +23,8 @@ const App = () => {
       .catch((err) => {
         console.log(err);
       });
+    // Set word to '' (cleaning the input)
+    setWord("");
   };
 
   return (
